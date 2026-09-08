00d99ddbb9954972c9f2599b11929dde5ee09311
SeleznevAS-dev <seleznev.as.dev@gmail.com>
2026-09-08 22:58:08 +0300

    Add handlers for validation and bad credentials

--- core/handlers.py
+++ core/handlers.py
@@ -1,13 +1,32 @@
-from fastapi import Request
+from fastapi import HTTPException, Request
+from fastapi.exceptions import RequestValidationError
+from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-from fastapi_csrf_protect.exceptions import CsrfProtectError
+from fastapi_users.router.common import ErrorCode
 
 
-def _add_csrf_exception_handler(app):
-    @app.exception_handler(CsrfProtectError)
-    async def csrf_protect_exception_handler(request: Request, exc: CsrfProtectError):
-        return JSONResponse(status_code=exc.status_code, content={"detail": exc.message})
+def _add_validation_exception_handler(app):
+    @app.exception_handler(RequestValidationError)
+    async def validation_exception_handler(request: Request, exc: RequestValidationError):
+        errors = exc.errors()
+
+        invalid_body = any(
+            error["type"] == "json_invalid" or (error["loc"] == ("body",) and isinstance(error.get("input"), bytes))
+            for error in errors
+        )
+
+        status_code = 400 if invalid_body else 422
+
+        return JSONResponse(status_code=status_code, content={"detail": jsonable_encoder(errors)})
+
+
+def _add_bad_credentials_exception_handler(app):
+    @app.exception_handler(HTTPException)
+    async def http_exception_handler(request: Request, exc: HTTPException):
+        status_code = 401 if exc.detail == ErrorCode.LOGIN_BAD_CREDENTIALS else exc.status_code
+        return JSONResponse(status_code=status_code, content={"detail": exc.detail})
 
 
 def add_handlers(app):
-    _add_csrf_exception_handler(app)
+    _add_validation_exception_handler(app)
+    _add_bad_credentials_exception_handler(app)
